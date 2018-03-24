7f6a6e455253d48355860d247847806e391d28d4
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-03-24 19:11:26 +0500

    Выполнил 5_2c

---
+++ 05_basic_scripts/tast_5_2c.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+""" При запросе параметра, которого нет в словаре устройства, должно отображаться сообщение 'Такого параметра нет'
+Enter device name: r1
+Enter parameter name (ios,model,vendor,location,ip): io
+Такого параметра нет
+"""
+
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+
+device = london_co.get(input('Enter device name: '))  # Запросили ключ словаря через dict.get() и закинули его в device
+device_keys = ','.join(device)      # Разделяем каждый элемент строки запятыми, чтобы не было всяких кавычек и прочего
+
+# Методом setdefault() ищем ключ, если он есть, то заносим его значение в result, в противном случае
+# создаётся ключ со значением "такого параметра нет" и это значение попадает в result
+result = device.setdefault((input('Enter parameter name (' + device_keys + '):')), 'Такого параметра нет')
+
+print(result)
