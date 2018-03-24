9fb156b5e0ffd9cfdaf44db9bb52fdf7c7b205c7
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-03-24 18:25:50 +0500

    Выполнил 5_2b

---
+++ 05_basic_scripts/task_5_2b.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+""" При запросе параметра, должен отображаться список возможных параметров
+Enter device name: r1
+Enter parameter name (ios,model,vendor,location,ip): ip
+10.255.0.1
+"""
+
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+
+device = london_co.get(input('Enter device name: '))  # Запросили ключ словаря через dict.get() и закинули его в device
+device_keys = ','.join(device)      # Разделяем каждый элемент строки запятыми, чтобы не было всяких кавычек и прочего
+
+# device.get - значение словаря device. input() - показывает возможные ключи словар.
+print(device.get(input('Enter parameter name (' + device_keys + '):')))
+
