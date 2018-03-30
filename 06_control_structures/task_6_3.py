865998892334be265ba3674a9a0c78b8a57b80ba
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-03-30 11:09:22 +0500

    убрал бессмысленный перебор списка

--- 06_control_structures/task_6_3.py
+++ 06_control_structures/task_6_3.py
@@ -45,12 +45,11 @@ for intf_t, list_t in fast_int['trunk'].items():    # Перебор значе�
     print('interface FastEthernet' + intf_t)
     for command_t in trunk_template:    # Перебираем шаблон
         if command_t.endswith('allowed vlan'):  # Если заканчивается на 'allowed vlan', то
-            for k in list_t:    # переборка списка list_t. Upd:Переборку смысла делать нет. Нужно переделать!!!
-                if k == 'add':  # если в списке есть 'add', то...
+            if list_t[0] == 'add':  # если в списке есть 'add', то...
                     print(' {} add {}'.format(command_t, vlan_t))
-                elif k == 'del':
+            elif list_t[0] == 'del':
                     print(' {} remove {}'.format(command_t, vlan_t))
-                elif k == 'only':
+            elif list_t[0] == 'only':
                     print(' {} {}'.format(command_t, vlan_t))
         else:
             print(' {}'.format(command_t))
