9edf926974b4e763cb6783a549c79456fba29782
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-04-03 12:19:40 +0500

    Выполнил 7_3a

--- 07_files/task_7_3a.py
+++ 07_files/task_7_3a.py
@@ -3,10 +3,17 @@
     Отсортировать вывод по номеру VLAN
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+# решение я подсмотрел.
+
+result_list = []
 
 with open('CAM_table.txt', 'r') as list:
     for k in list:
         if '.' in k:    # если в строке есть точка, то
             vlan, mac, _, intf = k.split()  # присваиваем переменным элементы строки k.split()
-            print(vlan, mac, intf)
+            unsorted = '{vlan:6} {mac}   {intf}'.format(vlan=vlan, mac=mac, intf=intf) # форматированое присваивание переменной элементов
+            result_list.append(unsorted)    # добавлять к result_list вёс новые и новые элементы
 
+result_list.sort()      # сортируем
+for k in result_list:   # выводим каждый элемент в новой строке.
+    print(k)
