9d3c8b4e2b2890309a6dbde9a96b0d7b49a7cade
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-04-04 13:20:00 +0500

    Убрал ненужные операции

--- 07_files/task_7_3b.py
+++ 07_files/task_7_3b.py
@@ -8,7 +8,6 @@ vlan_num = input('Введиде номер VLAN: ')
 with open('CAM_table.txt', 'r') as list:
     for k in list:  # перебираем каждую строку в файле
         if '.' in k: # если есть точка, то
-            vlan, mac, _, intf = k.split()
-            result = vlan,mac,intf   # закинули в result другие переменные
+            result = k.split()   # закинули в result содержимое строки
             if result[0] == vlan_num:   # если нулевой элемент равен введённому влану, что выводим 1 и 2 элемены
-                print(result[1], result[2])
+                print(result[1], result[3])
