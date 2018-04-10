035e4b8ee922f22df0a41444de6541d0c2f492c6
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-04-10 13:32:05 +0500

    пока не понял

---
+++ 09_functions/task_9_5.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 9.4a
+
+Задача такая же, как и задании 9.4.
+Проверить работу функции надо на примере файла config_r1.txt
+
+Обратите внимание на конфигурационный файл.
+В нем есть разделы с большей вложенностью, например, разделы:
+* interface Ethernet0/3.100
+* router bgp 100
+
+Надо чтобы функция config_to_dict обрабатывала следующий уровень вложенности.
+При этом, не привязываясь к конкретным разделам.
+Она должна быть универсальной, и сработать, если это будут другие разделы.
+
+Если уровня вложенности два:
+* то команды верхнего уровня будут ключами словаря,
+* а команды подуровней - списками
+
+Если уровня вложенности три:
+* самый вложенный уровень должен быть списком,
+* а остальные - словарями.
+
+На примере interface Ethernet0/3.100:
+
+{'interface Ethernet0/3.100':{
+               'encapsulation dot1Q 100':[],
+               'xconnect 10.2.2.2 12100 encapsulation mpls':
+                   ['backup peer 10.4.4.4 14100',
+                    'backup delay 1 1']}}
+
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+from pprint import pprint
+
+ignore = ['duplex', 'alias', 'Current configuration']
+
+
+def ignore_command(command, ignore):
+    '''
+    Функция проверяет содержится ли в команде слово из списка ignore.
+
+    command - строка. Команда, которую надо проверить
+    ignore - список. Список слов
+
+    Возвращает
+    * True, если в команде содержится слово из списка ignore
+    * False - если нет
+    '''
+    return any(word in command for word in ignore)
+
+def config(open_cfg):
+    with open(open_cfg) as src:
+        result = {}
+        for line in src:
+            if ignore_command(command=line, ignore=ignore) or line.startswith('!'): # если это есть, то продолжаем
+                continue
+            if not line.startswith(' '):    # если не содержит проблем в начале строки, то
+                key = line.strip()  # ключ - строка, в которой убраны whitespace-символы
+                result[key] = []    # закидываем в словарь ключ с пустным значением
+            elif
+
+
+
+                result[key].append(line.strip())    # иначе строка является значением.
+
+    return result
+
+pprint(config('config_sw1.txt'))
+
+
