cf227ed9d8b85652acb3f42763f34d4fbb2c498b
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-04-14 10:23:27 +0500

    выполнил 11_1

--- 11_modules/task_11_1.py
+++ 11_modules/task_11_1.py
@@ -30,10 +30,22 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 from pprint import pprint
 
-with open('sw1_sh_cdp_neighbors.txt') as src:
-    for line in src:
-        result = {}
-        line = line.split('\n')
-        if '>' in line:
-            localdev = line[0].split('>')[0]
-            print(localdev)
+
+def parse_cdp_neighbors(src_file):
+    list = src_file.split('\n')
+    local_device = list[0].split('>')[0]    # нулевую строку разделяем и берём первый элемент
+    result = {}
+    for line in list:
+        if '/' in line:
+            remote_device = line.split()[0]
+            local_interface = line.split()[1] + line.split()[2]     # собираем интерфейс вида fa0/0
+            remote_interface = line.split()[-2] + line.split()[-1]  # собираем интерфейс вида fa0/0.
+            result[local_device, local_interface] = (remote_device, remote_interface)
+    return result
+
+
+# Если функция запускается как самостоятельный элемент, а не импортируется куда-то как модуль, то выполняется следующее
+if __name__ == '__main__':
+    with open('sw1_sh_cdp_neighbors.txt') as src:  # открывает указанный файл
+        src_file = src.read()  # считываем открытый файл и кидаем его в переменную
+    pprint(parse_cdp_neighbors(src_file))  # выводим результат работы функции, с аргументом file
