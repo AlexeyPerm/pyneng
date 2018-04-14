7827a92e8a18f1c2249eafc1ce46daac924e2ca7
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-04-14 13:47:19 +0500

    начал 11_2.
    Какая-то беда с пакетом

--- 11_modules/task_11_2.py
+++ 11_modules/task_11_2.py
@@ -26,3 +26,11 @@
 > pip install graphviz
 
 '''
+
+from draw_network_graph import draw_topology
+from task_11_1 import parse_cdp_neighbors
+
+with open('sw1_sh_cdp_neighbors.txt') as src:
+    src_file = src.read()
+
+draw_topology(parse_cdp_neighbors(src_file))
