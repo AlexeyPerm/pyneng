59f729792b901b02a67e0d34918f92a0264f9f7f
AlexeyPerm <fx5600@mail.ru>
2018-04-23 11:22:19 +0500

    тестировочный файлик

--- 12_useful_modules/test.py
+++ 12_useful_modules/test.py
@@ -1,21 +1,27 @@
 # ip = ['8.8.8.8', '254.254.254.254', '1.1.1.1-10', '254.254.253.254']
 
-ip_list = ['8.8.8.8-9','1.2.3.4-10', '254.254.254.254', '127.0.0.1-10']
-i = len(ip_list)
-while i > 0:
-    for ip in ip_list:
-        if '-' in ip.rsplit('.', maxsplit=1)[1]:
-            ip_list.remove(ip)
-            ip_list1 = []
-            ip_first = ip.rsplit('.', maxsplit=1)[0]
-            ip_end = ip.rsplit('.', maxsplit=1)[1].split('-')
-            octet_range = [i for i in range(int(ip_end[0]), int(ip_end[1]) + 1)]
-            #print(octet_range)
-            for k in octet_range:
-                ip_list.append(ip_first + '.' + str(k))
+ip_list = ['10.1.1.1-10','127.0.0.1-127.0.0.5']
+#ip_list = ['10.1.1.1-10']
+for ip in ip_list:
+    if '-' in ip:
+        ip_begin = ip.split('-')[0].rsplit('.', maxsplit=1)[0]
+        if len(ip.split('-')[1]) > 3:
+            ip = ip.split('-')
+            rng = []
+            for j in ip:
+                rng.append(j.rsplit('.', maxsplit=1)[-1])
+        else:
+            rng = ip.rsplit('.', maxsplit=1)[1].split('-')
+        octet_range = [i for i in range(int(rng[0]), int(rng[1]) + 1)]
 
-    i -= 1
-print(ip_list)
+    # if '-' in ip and len(ip.split('-')[1]) > 3:
+    #     ip_1, ip_2 = ip.split('-')
+    #     last_octet1 = ip_1.split('.')[-1]
+    #     last_octet2 = ip_2.split('.')[-1]
+    # elif '-' in ip:
+    #     ip_begin = ip.rsplit('.', maxsplit=1)[0]
+    #     octet_range = ip.rsplit('.', maxsplit=1)[1].split('-')
+
 # def check_list_ip(ip_list):
 #     for ip in ip_list:
 #         if '-' in ip.rsplit('.', maxsplit=1)[1]:
