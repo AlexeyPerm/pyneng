8e0a79cf7ff5048a6597057d0d31bbd3a40797f8
AlexeyPerm <fx5600@mail.ru>
2018-04-23 12:51:02 +0500

    Выполнил 12_3

--- 12_useful_modules/task_12_3.py
+++ 12_useful_modules/task_12_3.py
@@ -21,3 +21,19 @@ Reachable    Unreachable
 То есть, до выполнения функции и после списки должны выглядеть одинаково.
 
 '''
+
+from task_12_1 import check_ip_addresses
+from task_12_2 import check_list_ip
+from tabulate import tabulate
+
+
+def ip_table(avail, unavail):
+    ip_dict = {'Reachable': avail, 'Unreachable': unavail}
+
+    return tabulate(ip_dict, headers='keys')
+
+
+if __name__ == '__main__':
+    ip_list = ['8.8.8.8', '1.1.1.1-1.1.1.3', '127.0.0.1-3', '2.2.2.2-2.2.2.4']
+    result = check_ip_addresses(check_list_ip(ip_list))
+    print(ip_table(result[0], result[1]))
