f785a18cfa0c5319dc2771765059bd59cc649db8
AlexeyPerm <fx5600@mail.ru>
2018-04-23 11:23:15 +0500

    Подвёл ко всем условиям задачи

--- 12_useful_modules/task_12_2.py
+++ 12_useful_modules/task_12_2.py
@@ -32,49 +32,40 @@ IP-адреса могут быть в формате:
 
 # Для Python3.6 и выше, так как используются f-строки
 
-import subprocess
+
+from task_12_1 import check_ip_addresses
 
 
 def check_list_ip(ip_list):
     """ Функция проверяет, присутствует ли диапазон айпишников
-    в каком-либо элементе списка. While используется, чтобы
-    делать проверку для каждого элемента списка, так как диапазонов
-    может быть несколько. Возвращает обновлённый список.
-    Пример: Было ip_list = ['12.3.4.5-7]
-            Стало ip_list = ['12.3.4.5', '12.3.4.6', '12.3.4.7']
+    в каком-либо элементе списка и, если он есть, то "разворачивает его".
+    While используется, чтобы делать проверку для каждого элемента списка,
+    так как диапазонов может быть несколько. Возвращает обновлённый список.
+    Пример: Было ip_list = ['12.3.4.5-7', '127.0.0.1-127.0.0.3]
+            Стало ip_list = ['12.3.4.5', '12.3.4.6', '12.3.4.7', '127.0.0.1', '127.0.0.2', '127.0.0.3']
 
     """
+
     i = len(ip_list)
     while i > 0:
         for ip in ip_list:
-            if '-' in ip.rsplit('.', maxsplit=1)[1]:    # сплитить начинаем справа и только один раз.
-                ip_list.remove(ip)  # удаляем прошедший провеку элемент,
-                ip_first = ip.rsplit('.', maxsplit=1)[0]
-                ip_end = ip.rsplit('.', maxsplit=1)[1].split('-')
-                octet_range = [i for i in range(int(ip_end[0]), int(ip_end[1]) + 1)]    # "разбиваем" диапазон
+            if '-' in ip:
+                ip_begin = ip.split('-')[0].rsplit('.', maxsplit=1)[0]
+                if len(ip.split('-')[1]) > 3:
+                    ip = ip.split('-')
+                    rng = []
+                    for j in ip:
+                        rng.append(j.rsplit('.', maxsplit=1)[-1])
+                else:
+                    rng = ip.rsplit('.', maxsplit=1)[1].split('-')
+                octet_range = [i for i in range(int(rng[0]), int(rng[1]) + 1)]
                 for k in octet_range:
-                    ip_list.append(ip_first + '.' + str(k))
+                    ip_list.append(ip_begin + '.' + str(k))
         i -= 1
     return ip_list
 
 
-def check_ip_addresses(ip_address):
-    ping_ok = []
-    ping_not_ok = []
-
-    for k in ip_address:
-        reply = subprocess.run(['ping', '-c', '1', k],
-                               stdout=subprocess.DEVNULL,
-                               stderr=subprocess.DEVNULL)
-        if reply.returncode == 0:
-            ping_ok.append(k)
-        elif reply.returncode == 1:
-            ping_not_ok.append(k)
-    return ping_ok, ping_not_ok
-
-
 if __name__ == '__main__':
-    # ip_list = ['8.8.8.8', '254.254.254.254', '1.1.1.1', '127.0.0.1-4', '254.254.253.254']
-    ip_list = ['8.8.8.8', '254.254.254.250-252', '1.1.1.1-3', '127.0.0.1-4', '254.254.253.255']
+    ip_list = ['8.8.8.8', '254.254.254.250-252', '1.1.1.1-1.1.1.3', '127.0.0.1-3', '254.254.253.255']
     result = check_ip_addresses(check_list_ip(ip_list))
     print(f'Available IP: {result[0]} \nUnavailable IP: {result[1]}')
