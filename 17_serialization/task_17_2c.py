3cb5fe762baf9c06c1904973b7407a923fa86fd9
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2018-05-09 21:23:04 +0500

    Выполнил 17_2c

--- 17_serialization/task_17_2c.py
+++ 17_serialization/task_17_2c.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 17.2c
 
 С помощью функции draw_topology из файла draw_network_graph.py
@@ -28,4 +28,27 @@
 > И модуль python для работы с graphviz:
 > pip install graphviz
 
-'''
+"""
+from draw_network_graph import draw_topology
+import yaml
+from pprint import pprint
+
+result = {}
+with open("topology.yaml") as f:
+    templates = yaml.load(f)
+
+# Вытаскиваем ключи и значения словарей, чтобы сделать нужного вида словарь для draw_topology. Пока только это придумал.
+for k in templates:
+    for local_device, a in k.items():
+        for local_int, b in a.items():
+            for remote_dev, remote_int in b.items():
+                break
+            result[local_device, local_int] = (remote_dev, remote_int)
+
+# проверка на дублирование линков.
+for j in list(result.values()):
+    if j in result.keys() and result[j] in result.keys():
+        del(result[j])
+
+draw_topology(result)
+
