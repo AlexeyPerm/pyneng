378477b2452d496a917afd42e752c598674d9510
AlexeyPerm <fx5600@mail.ru>
2018-05-29 19:50:15 +0500

    test

--- 18_db/task_18_1/create_db.py
+++ 18_db/task_18_1/create_db.py
@@ -25,10 +25,7 @@ import sqlite3
 def create_db():
     db_filename = 'dhcp_snooping.db'
     schema_filename = 'dhcp_snooping_schema.sql'
-
-
     db_exists = os.path.exists(db_filename)
-
     conn = sqlite3.connect(db_filename)
 
     if not db_exists:
